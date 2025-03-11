19a61bdcdab8247cba2c2f70008398e3ce739740
Sagar Patil <netrasagar12mati@gmail.com>
2025-03-11 23:54:39 +0530

    pdf loading in langchain

---
+++ loading_documents/load_document.py
@@ -0,0 +1,8 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+file_path = "G:\genai\langchain_basics_genai\loading_documents\sample_document.pdf"
+loader = PyPDFLoader(file_path)
+
+docs = loader.load()
+
+print(len(docs))
