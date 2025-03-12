ce1c1dbdc022b34295836700d7de64f2b72790ff
Sagar Patil <netrasagar12mati@gmail.com>
2025-03-12 23:34:11 +0530

    document loading update 12 march 2025

--- loading_documents/load_document.py
+++ loading_documents/load_document.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 
-file_path = "G:\genai\langchain_basics_genai\loading_documents\sample_document.pdf"
+file_path = "G:\genai\langchain_basics_genai\loading_documents\sample_document1.pdf"
 loader = PyPDFLoader(file_path)
 
 docs = loader.load()
